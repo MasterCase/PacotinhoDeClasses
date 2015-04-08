d263de3229b14e05868d4086d3c526783f28a6c6
githubgabriel <tx.gabrielbarbosa@gmail.com>
2015-04-08 14:11:06 -0400

    fix functions in plugin :smile:

--- javascript/JqueryPlugins/checkAllBox.plugin.js
+++ javascript/JqueryPlugins/checkAllBox.plugin.js
@@ -1,82 +1,81 @@
 (function ( $ ) {
-	
-	/**
-	*	Plugin Jquery para selecionar multiplos checkboxs
-	*   Graças a Jesus Cristo
-	*	@author Gabriel A. Barbosa <tx.gabrielbarbosa@gmail.com>
-	*	@version 1.0.0
-	*/
+
+    /**
+     *	Plugin Jquery para selecionar multiplos checkboxs
+     *   Graças a Jesus Cristo
+     *	@author Gabriel A. Barbosa <tx.gabrielbarbosa@gmail.com>
+     *	@version 1.0.0
+     */
     $.fn.checkAllBox = function( options ) {
-		
-		/**
-		*	Settings Default
-		*/
-		var settings = $.extend({
-			forceChecked: null,
-			debug: false,
-			eventChecked: function() {
-					debugx("Event: eventChecked Iniciado..!");
-			},
-			eventUnChecked: function() { 
-					debugx("Event: eventUnChecked Iniciado..!");
-			}
-		}, options);
-		
-		/**
-		*	Function para escrever no console.log
-		*/
-		debugx = function(text) { 
-			if(settings.debug) { 
-				console.log(text);
-			}
-		}
-		debugx("========= checkAllBox ========= init()");
 
-		/**
-		*	Verifica se existe algum checkbox selecionado
-		*	Caso exista retorna: True
-		*   Caso não exista retorna: False
-		*/
-		isChecked = function(obj) {
-			var resultado = false;
-			$(obj).each(function(){ 
-				var temp = $(this).is(":checked");
-				if(temp) { resultado = true; }
-			});
-			debugx("isChecked(): " + resultado);
-			return resultado;
-		};
+        /**
+         *	Settings Default
+         */
+        var settings = $.extend({
+            forceChecked: null,
+            debug: false,
+            eventChecked: function() {
+                debugx("Event: eventChecked Iniciado..!");
+            },
+            eventUnChecked: function() {
+                debugx("Event: eventUnChecked Iniciado..!");
+            }
+        }, options);
 
-		/**
-		*	Seta valor de todos checkbox (True or False)
-		*/
-		setAllCheckBox = function (obj, valor) { 
-			if(valor) { 
-				$(obj).each(function(){ $(obj).attr("Checked", valor); });
-			} else { 
-				$(obj).each(function(){ $(obj).removeAttr("Checked"); });
-			}
-		}
-		
-		/**
-		*	Init das operações
-		*/
-		if(settings.forceChecked != null) { 
-			if(settings.forceChecked) { 
-				setAllCheckBox(this, true);
-				settings.eventChecked();
-			} else { 
-				setAllCheckBox(this, false);
-				settings.eventUnChecked();
-			}
-		} else if(!isChecked(this)) { 
-			setAllCheckBox(this, true);
-			settings.eventChecked();
-		} else { 
-			setAllCheckBox(this, false);
-			settings.eventUnChecked();
-		}
-	
-	};
+        /**
+         *	Function para escrever no console.log
+         */
+        debugx = function(text) {
+            if(settings.debug) {
+                console.log(text);
+            }
+        }
+        debugx("========= checkAllBox ========= init()");
+
+        /**
+         *	Pega status do botão principal
+        */
+        var isChecked = $(this).prop("checked");
+        debugx("this.prop(checked) =  " + isChecked);
+        if(isChecked) {
+            isChecked = true;
+        } else {
+            isChecked = false;
+        }
+        debugx("var isChecked = "+isChecked);
+
+
+        /**
+         *	Seta valor de todos checkbox (True or False)
+         */
+        setAllCheckBox = function (obj, valor) {
+            debugx("function setAllCheckBox(this, " + valor + ");");
+            if(valor) {
+                $(obj).each(function(){ $(this).prop("checked", valor); });
+            } else {
+                $(obj).each(function(){  $(this).prop("checked", valor);  });
+            }
+        }
+
+        /**
+         *	Init das operações
+         */
+        if(settings.forceChecked != null) {
+            if(settings.forceChecked) {
+                setAllCheckBox(this, true);
+                settings.eventChecked();
+            } else {
+                setAllCheckBox(this, false);
+                settings.eventUnChecked();
+            }
+        } else if(isChecked) {
+            setAllCheckBox(this, true);
+            settings.eventChecked();
+        } else {
+            setAllCheckBox(this, false);
+            settings.eventUnChecked();
+        }
+
+    };
 
 }( jQuery ));
